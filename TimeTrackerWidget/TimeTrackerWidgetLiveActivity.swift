5173f80494b4ad5e1f1ee4de4c202b824f3ca0bc
uzuleeeee <ulee36@gatech.edu>
2025-12-18 00:18:16 -0500

    [Refactor] Separate widget attributes to TimeTrackerAttributes

--- TimeTrackerWidget/TimeTrackerWidgetLiveActivity.swift
+++ TimeTrackerWidget/TimeTrackerWidgetLiveActivity.swift
@@ -9,20 +9,6 @@ import ActivityKit
 import WidgetKit
 import SwiftUI
 
-struct TimeTrackerWidgetAttributes: ActivityAttributes {
-    public struct ContentState: Codable, Hashable {
-        // Dynamic stateful properties about your activity go here!
-        let categoryName: String
-        let description: String?
-        let iconName: String
-        let startTime: Date?
-        let colorHex: String
-    }
-
-    // Fixed non-changing properties about your activity go here!
-    let id: UUID
-}
-
 struct TimeTrackerWidgetLiveActivity: Widget {
     var body: some WidgetConfiguration {
         ActivityConfiguration(for: TimeTrackerWidgetAttributes.self) { context in
