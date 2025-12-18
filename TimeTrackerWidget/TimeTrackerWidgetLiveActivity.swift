810d905825efbe3e58e5c855ec12f19a6dd9a777
uzuleeeee <ulee36@gatech.edu>
2025-12-18 00:11:55 -0500

    Implement live activity UI

--- TimeTrackerWidget/TimeTrackerWidgetLiveActivity.swift
+++ TimeTrackerWidget/TimeTrackerWidgetLiveActivity.swift
@@ -12,8 +12,10 @@ import SwiftUI
 struct TimeTrackerWidgetAttributes: ActivityAttributes {
     public struct ContentState: Codable, Hashable {
         // Dynamic stateful properties about your activity go here!
+        let categoryName: String
+        let description: String?
         let iconName: String
-        let startTime: Date
+        let startTime: Date?
         let colorHex: String
     }
 
@@ -25,11 +27,46 @@ struct TimeTrackerWidgetLiveActivity: Widget {
     var body: some WidgetConfiguration {
         ActivityConfiguration(for: TimeTrackerWidgetAttributes.self) { context in
             // Lock screen/banner UI goes here
-            VStack {
-                Text("Hello \(context.state.emoji)")
+            HStack(spacing: 12) {
+                // Icon
+                Image(systemName: context.state.iconName)
+                    .font(.title2)
+                    .foregroundStyle(Color(hex: context.state.colorHex))
+                    .padding(10)
+                    .background(Color(hex: context.state.colorHex).opacity(0.2))
+                    .clipShape(Circle())
+                
+                // Text Details
+                VStack(alignment: .leading) {
+                    Text(context.state.categoryName)
+                        .font(.headline)
+                        .foregroundStyle(.primary)
+                    if let description = context.state.description, !description.isEmpty {
+                        Text(description)
+                            .font(.caption)
+                            .foregroundStyle(.secondary)
+                    }
+                }
+                
+                Spacer()
+                
+                if let startTime = context.state.startTime {
+                    Text(startTime, style: .timer)
+                        .font(.system(.title, design: .monospaced))
+                        .bold()
+                        .foregroundStyle(Color(hex: context.state.colorHex))
+                        .multilineTextAlignment(.trailing)
+                        .frame(alignment: .trailing)
+                } else {
+                    Text("--:--")
+                        .font(.system(.title, design: .monospaced))
+                        .bold()
+                        .multilineTextAlignment(.trailing)
+                        .foregroundStyle(.gray)
+                }
             }
-            .activityBackgroundTint(Color.cyan)
-            .activitySystemActionForegroundColor(Color.black)
+            .frame(maxWidth: .infinity)
+            .padding(.horizontal)
 
         } dynamicIsland: { context in
             DynamicIsland {
@@ -42,15 +79,15 @@ struct TimeTrackerWidgetLiveActivity: Widget {
                     Text("Trailing")
                 }
                 DynamicIslandExpandedRegion(.bottom) {
-                    Text("Bottom \(context.state.emoji)")
+                    Text("Bottom")
                     // more content
                 }
             } compactLeading: {
                 Text("L")
             } compactTrailing: {
-                Text("T \(context.state.emoji)")
+                Text("T")
             } minimal: {
-                Text(context.state.emoji)
+                Text("M")
             }
             .widgetURL(URL(string: "http://www.apple.com"))
             .keylineTint(Color.red)
@@ -60,23 +97,26 @@ struct TimeTrackerWidgetLiveActivity: Widget {
 
 extension TimeTrackerWidgetAttributes {
     fileprivate static var preview: TimeTrackerWidgetAttributes {
-        TimeTrackerWidgetAttributes(name: "World")
+        TimeTrackerWidgetAttributes(
+            id: UUID()
+        )
     }
 }
 
 extension TimeTrackerWidgetAttributes.ContentState {
-    fileprivate static var smiley: TimeTrackerWidgetAttributes.ContentState {
-        TimeTrackerWidgetAttributes.ContentState(emoji: "😀")
-     }
-     
-     fileprivate static var starEyes: TimeTrackerWidgetAttributes.ContentState {
-         TimeTrackerWidgetAttributes.ContentState(emoji: "🤩")
-     }
+    fileprivate static var running: TimeTrackerWidgetAttributes.ContentState {
+        TimeTrackerWidgetAttributes.ContentState(
+            categoryName: "Work",
+            description: nil,
+            iconName: "briefcase.fill",
+            startTime: Date().addingTimeInterval(-900),
+            colorHex: "007AFF"
+        )
+    }
 }
 
 #Preview("Notification", as: .content, using: TimeTrackerWidgetAttributes.preview) {
    TimeTrackerWidgetLiveActivity()
 } contentStates: {
-    TimeTrackerWidgetAttributes.ContentState.smiley
-    TimeTrackerWidgetAttributes.ContentState.starEyes
+    .running
 }
