a9021d5496ba6c683caf663fb762e9d03f5e7422
uzuleeeee <ulee36@gatech.edu>
2026-01-03 02:14:14 -0500

    Add opacity animation to bouncy button style

--- Time Tracker/UI/Buttons/BouncyButtonStyle.swift
+++ Time Tracker/UI/Buttons/BouncyButtonStyle.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct BouncyButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
-            .opacity(1.0)
-            .animation(.easeInOut(duration: 0.1), value: configuration.isPressed)
+            .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
+            .opacity(configuration.isPressed ? 0.6 : 1.0)
+            .animation(.spring(response: 0.3, dampingFraction: 0.6), value: configuration.isPressed)
     }
 }
 
