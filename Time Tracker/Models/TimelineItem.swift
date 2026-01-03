8f3cf5e38221b2cb0eeb7188d498deed617e60a6
uzuleeeee <ulee36@gatech.edu>
2026-01-03 02:29:31 -0500

    Add dateIndicator case to TimelineItem

--- Time Tracker/Models/TimelineItem.swift
+++ Time Tracker/Models/TimelineItem.swift
@@ -11,11 +11,13 @@ import SwiftUI
 enum TimelineItem: Identifiable {
     case activity(ActivityUIModel)
     case gap(GapUIModel)
+    case dateIndicator(Date)
     
     var id: String {
         switch self {
         case .activity(let model): return model.id.uuidString
         case .gap(let model): return model.id
+        case .dateIndicator(let date): return "date-\(date.timeIntervalSince1970)"
         }
     }
 }
